a7c0a702c31ba919d3eae503c795c3d2b42de83c
R-o-t-I <rulers.oftheinfinite@mail.ru>
2021-11-14 16:04:50 +0300

    Updates

---
+++ precache-manifest.b59bc1551213727edf53705aff092972.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9bb866e7e807c625cac5",
+    "url": "/acquaintance/static/css/main.7034bf19.chunk.css"
+  },
+  {
+    "revision": "9bb866e7e807c625cac5",
+    "url": "/acquaintance/static/js/main.8cd44714.chunk.js"
+  },
+  {
+    "revision": "360cc2fe9bf206600ae4",
+    "url": "/acquaintance/static/js/runtime~main.f710001a.js"
+  },
+  {
+    "revision": "224cd4c1e7fb0a370d87",
+    "url": "/acquaintance/static/css/2.e0c11635.chunk.css"
+  },
+  {
+    "revision": "224cd4c1e7fb0a370d87",
+    "url": "/acquaintance/static/js/2.0ac181a4.chunk.js"
+  },
+  {
+    "revision": "0221da9d4f16c519d8d8",
+    "url": "/acquaintance/static/js/3.0569262b.chunk.js"
+  },
+  {
+    "revision": "370ac7ca980d14a70d80",
+    "url": "/acquaintance/static/js/4.7ed40279.chunk.js"
+  },
+  {
+    "revision": "c88ee5225fe2d7cea42a088b4a87ca79",
+    "url": "/acquaintance/index.html"
+  }
+];
