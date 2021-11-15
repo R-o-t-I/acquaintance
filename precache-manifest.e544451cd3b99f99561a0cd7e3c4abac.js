ca29b5aed70eea40c2bf662205e5383783c78dc3
R-o-t-I <rulers.oftheinfinite@mail.ru>
2021-11-15 19:25:31 +0300

    Updates

---
+++ precache-manifest.e544451cd3b99f99561a0cd7e3c4abac.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1bae354913e3a8df1e57",
+    "url": "/acquaintance/static/css/main.5b7acc47.chunk.css"
+  },
+  {
+    "revision": "1bae354913e3a8df1e57",
+    "url": "/acquaintance/static/js/main.f7a8881f.chunk.js"
+  },
+  {
+    "revision": "da1cd2865006ae3f3bc3",
+    "url": "/acquaintance/static/js/runtime~main.9e15004b.js"
+  },
+  {
+    "revision": "16d13f123f31ed580db7",
+    "url": "/acquaintance/static/css/2.1a271433.chunk.css"
+  },
+  {
+    "revision": "16d13f123f31ed580db7",
+    "url": "/acquaintance/static/js/2.c211d3f3.chunk.js"
+  },
+  {
+    "revision": "5e101adfef107e9502dc",
+    "url": "/acquaintance/static/js/3.1e628bfe.chunk.js"
+  },
+  {
+    "revision": "afef07cda88e15e314d5",
+    "url": "/acquaintance/static/js/4.d2698c75.chunk.js"
+  },
+  {
+    "revision": "e243897179d08bd39fe7da905616b3d2",
+    "url": "/acquaintance/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/acquaintance/precache-manifest.db076ceba21ff8607050f05f6be1ba2d.js"
+  "/acquaintance/precache-manifest.e544451cd3b99f99561a0cd7e3c4abac.js"
 );
 
 workbox.clientsClaim();
